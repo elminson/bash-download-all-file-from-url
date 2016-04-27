589ca5d956467d7261d1fb43b37972ab93709f55
Elminson De Oleo Baez <edeoleo@gmail.com>
2016-04-27 16:41:47 -0400

    Update files_downloader.sh

--- files_downloader.sh
+++ files_downloader.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Created By Elminson De Oleo Baez
+# edeoleo@gmail.com
+# 04/27/2016 NYC
 display_usage() {
         echo "This script must be run with super-user privileges."
         echo -e "\nUsage:\n$0 [url] [file_format] \n"
@@ -18,7 +21,6 @@ NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 NEW_UUID_FOLDER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 echo "/tmp/$NEW_UUID"
 wget -q -O - "$@" $1  |  sed -ne 's/.*\(http[^"]*\).*/\1/p' | grep $2 >> /tmp/$NEW_UUID
-#cat /tmp/$NEW_UUID
 mkdir /tmp/$NEW_UUID_FOLDER
 cd /tmp/$NEW_UUID_FOLDER
 for i in $( cat /tmp/$NEW_UUID ); do wget $i; done
